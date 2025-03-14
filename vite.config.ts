fd85e31e052df626de1cfa3c9ec29226cdab035c
bonnykato <6onnykato@gmail.com>
2025-03-14 13:54:53 +0300

    chore: remove pnpm-lock.yaml
    - Deleted pnpm-lock.yaml to clean up unnecessary lockfile.
    - This change ensures the repository remains lightweight and avoids redundant files.
    
    Refs: #2

--- vite.config.ts
+++ vite.config.ts
@@ -15,21 +15,11 @@ export default defineConfig({
     lib: {
       entry: resolve(fileURLToPath(new URL('.', import.meta.url)), 'src/index.ts'),
       name: 'R3Utils',
-      fileName: (format) => `r3-utils.${format}.js`,
+      fileName: "r3-utils",
+      formats: ["es"]
     },
     rollupOptions: {
-      // Make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ['react', 'react-dom', 'react-router'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          'react-router': 'ReactRouter',
-        },
-      },
     },
   },
 });
