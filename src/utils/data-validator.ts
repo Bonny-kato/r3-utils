96113cd2e1a0c5bad6eea4fc404d53df5a46f06a
bonny-kato <6onnykato@gmail.com>
2025-10-13 22:25:21 +0300

    docs: update README for formatting and remove `constants` references
    - adjust line breaks for improved readability
    - remove `constants` module mentions from documentation

--- src/utils/data-validator.ts
+++ src/utils/data-validator.ts
@@ -1,7 +1,7 @@
 import { ZodError, ZodSchema } from "zod";
+import { HTTP_NOT_ACCEPTABLE } from "~/http-client";
 import { checkIsDevMode } from "./check-is-dev-mode";
 import { throwError } from "./error-utils";
-import { HTTP_NOT_ACCEPTABLE } from "~/http-client";
 
 type FormDataError<T> = Partial<Record<keyof T, string>>;
 type ValidateData<T> = {
